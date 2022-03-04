733c33642567ea06664d88423b16d6808d6645c4
NolanPG <84996426+NolanPG@users.noreply.github.com>
2022-03-03 20:33:43 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -34,17 +34,26 @@ async def resizer(client, message):
                                         replied_message.video.file_name)
     resized_video = download[::-1].replace(".", f" {requested_height}p."[::-1], 1)[::-1]
 
-    #try:
-    clip = mp.VideoFileClip(download)
-    clip_resized = clip.resize(height=int(requested_height))
-    """changing the height (According to moviePy documentation The width is then computed so that the
-    width/height ratio is conserved.)"""
-    clip_resized.write_videofile(resized_video, codec=None)
+    if resized_video.endswith(".mp4"):
+        codec = 'mpeg4'
+    elif resized_video.endswith(".ogv"):
+        codec = 'libvorbis'
+    elif resized_video.endswith(".webm"):
+        codec = 'libvpx'
+    else:
+        codec = None
 
-    await bot.send_video(chat_id=message.chat.id, video=resized_video)
+    try:
+        clip = mp.VideoFileClip(download)
+        clip_resized = clip.resize(height=int(requested_height))
+        """changing the height (According to moviePy documentation The width is then computed so that the
+        width/height ratio is conserved.)"""
+        clip_resized.write_videofile(resized_video, codec=codec)
 
-    #except ValueError:
-        #await bot.send_message(chat_id=message.chat.id,
-                         #text='Altura no permitida')
+        await bot.send_video(chat_id=message.chat.id, video=resized_video)
+
+    except ValueError:
+        await bot.send_message(chat_id=message.chat.id,
+                         text='Altura no permitida')
 
 bot.run()
