a23b28c800835a1b225633bcded53078b172ed95
NolanPG <84996426+NolanPG@users.noreply.github.com>
2022-04-09 04:06:25 -0400

    Changing to ffmpeg and Telethon

--- main.py
+++ main.py
@@ -1,95 +1,30 @@
-import moviepy.editor as mp
-from pyrogram import Client, filters
-import moviepy.video.io.ffmpeg_tools as ffmpeg
+from telethon import TelegramClient, events, utils
+import subprocess
+import pathlib
 
-bot = Client(
-    "rescaletestbot",
-    bot_token="5258246251:AAHloqD0rw0X_iH5JPEG27qoWC3k3JQ8Q2A"
-)
+api_id = 4482188
+api_hash = '0712c4aacf5e779ed9f55421c1813aed'
+bot_token = '5118931943:AAFtqUOmTordsumt0yKerHcVlz-sUjRuCDA'
 
-#Variables
-size = [360, 640]
-welcome_text_sp = "Bienvenida al bot :v"
-help_text_sp = "Explicación detallada del bot :v"
+bot = TelegramClient('rescaletestbot', api_id, api_hash).start(bot_token=bot_token)
 
 
-#Functions
-@bot.on_message(filters.command("start"))
-async def welcome(client, message):
-    await bot.send_message(chat_id=message.chat.id,
-                           text=welcome_text_sp,
-                           reply_to_message_id=message.message_id)
+@bot.on(events.NewMessage)
+async def resizer(event):
+    replied_message = await event.get_reply_message()
+    requested_height = event.raw_text.replace('/resize ', '')
+    file_name = replied_message.file.name
+    file_path = f'/app/downloads/{file_name}'
 
-
-@bot.on_message(filters.command("help"))
-async def helper(client, message):
-    await bot.send_message(chat_id=message.chat.id,
-                           text=help_text_sp,
-                           reply_to_message_id=message.message_id)
-
-
-@bot.on_message(filters.command(["resize"]))
-async def resizer(client, message):
-    replied_message = await bot.get_messages(chat_id=message.chat.id,
-                                             reply_to_message_ids=message.message_id)
-    requested_height = message.text.replace('/resize ', '')
-    if replied_message.video is not None:
-        if replied_message.video.file_name is None:
-            if replied_message.video.mime_type == 'video/mp4':
-                extension = '.mp4'
-            elif replied_message.video.mime_type == 'video/x-matroska':
-                extension = '.mkv'
-            elif replied_message.video.mime_type == 'video/x-msvideo':
-                extension = '.avi'
-            else:
-                extension = '.mp4'
-
-            video_name = 'video ' + str(replied_message.video.file_unique_id) + extension
-            download = await bot.download_media(message=replied_message.video.file_id,
-                                                file_name=video_name)
-        else:
-            download = await bot.download_media(message=replied_message.video.file_id,
-                                                file_name=replied_message.video.file_name)
-    else:
-        if replied_message.document.file_name is None:
-            if replied_message.document.mime_type == 'video/mp4':
-                extension = '.mp4'
-            elif replied_message.document.mime_type == 'video/x-matroska':
-                extension = '.mkv'
-            elif replied_message.document.mime_type == 'video/x-msvideo':
-                extension = '.avi'
-            else:
-                extension = '.mp4'
-
-            video_name = 'video ' + str(replied_message.document.file_unique_id) + extension
-            download = await bot.download_media(message=replied_message.document.file_id,
-                                                file_name=video_name)
-        else:
-            download = await bot.download_media(message=replied_message.document.file_id,
-                                                file_name=replied_message.document.file_name)
-
-    resized_video = download[::-1].replace(".", f" {requested_height}p."[::-1], 1)[::-1]
-
-    if str(resized_video).endswith('.mp4'):
-        codec = 'mpeg4'
-    elif str(resized_video).endswith('.ogv'):
-        codec = 'libvorbis'
-    elif str(resized_video).endswith('.webm'):
-        codec = 'libvpx'
-    elif str(resized_video).endswith('.mkv'):
-        codec = 'libx264'
-    else:
-        codec = None
+    download = await bot.download_media(message=replied_message, file=file_path)
+    extension = pathlib.Path(download).suffix
+    resized_video = str(download).replace(extension, f'_{requested_height}p.mp4')
 
     try:
-        clip = mp.VideoFileClip(download)
-        clip_resized = clip.resize(height=int(requested_height))
-        clip_resized.write_videofile(resized_video, codec=codec)
+        subprocess.run(f'ffmpeg -i {download} -vf scale=-1:{requested_height} -f mp4 {resized_video} -y', shell=True)
+    except Exception as e:
+        print(e)
+    
+    await bot.send_file(entity=event.chat_id, file=resized_video)
 
-    except ValueError:
-        await bot.send_message(chat_id=message.chat.id,
-                               text='Altura no permitida')
-
-    await bot.send_video(chat_id=message.chat.id, video=resized_video)
-
-bot.run()
+bot.run_until_disconnected()
