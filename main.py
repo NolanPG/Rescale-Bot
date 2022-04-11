c8a28bf63ab15b41d5a6ee72a9ab04642650d965
NolanPG <84996426+NolanPG@users.noreply.github.com>
2022-04-11 01:53:46 -0400

    Adding some feedback to the bot

--- main.py
+++ main.py
@@ -16,22 +16,28 @@ async def resizer(event):
     requested_height = event.raw_text.replace('/resize ', '')
     file_name = replied_message.file.name
     file_path = f'/app/downloads/{file_name}'
-
+	
+    response = await bot.send_message(event.chat_id, 'Downloading...')
     download = await bot.download_media(message=replied_message, file=file_path)
     extension = pathlib.Path(download).suffix
     resized_video = str(download).replace(extension, f'_{requested_height}p.mp4')
-
+    resized_video_name = pathlib.Path(resized_video).name
     def ffmpeg():
         try:
             return subprocess.run(f'ffmpeg -i {download} -vf scale=-1:{requested_height} -f mp4 {resized_video} -y', shell=True)
         except Exception as e:
             return e
 
-    
+    response.edit('Resizing video...')
     t = Thread(target=ffmpeg)
     t.start()
     t.join()
-
-    await bot.send_file(entity=event.chat_id, file=resized_video)
+	
+    response.edit('Uploading...')
+    
+    await bot.send_file(entity=event.chat_id, file=resized_video, caption=f'Finished the resizing of "{resized_video_name}"')
+    
+    await bot.delete_messages(entity=event.chat_id, message_ids=response)
+    
 
 bot.run_until_disconnected()
